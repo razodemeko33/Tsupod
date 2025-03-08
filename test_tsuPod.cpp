8856985db3bfd7822ed53ffe147ef3b10c8d8a37
QuantumCoder <160463322+razodemeko33@users.noreply.github.com>
2025-03-07 23:28:57 -0600

    Update test_tsuPod.cpp

--- test_tsuPod.cpp
+++ test_tsuPod.cpp
@@ -1,64 +1,94 @@
-/****************************************************Name:Date:Problem Number:Hours spent solving the problem:Instructor: Komogortsev, TSU*****************************************************/
+/****************************************************
+Name: [Your Name]
+Date: [Date]
+Problem Number: 4
+Hours spent solving the problem: [Hours]
+Instructor: Komogortsev, TSU
+*****************************************************/
 
 #include <iostream>
-#include <fstream>
-
 #include "tsuPod.h"
-
 using namespace std;
 
-int main() 
-{
-     
-    // Following code initializes the tsuPod 
-    initTsuPod(/*add nessesary parameters here*/);
+int main() {
+    Song tsuPod[NUM_SONGS];
+    int totalMemoryUsed = 0;
 
-
-    
-    // Following code lists all songs located in tsuPod memeory 
-//The word “empty” indicates empty slot in the playlist rather than name of the song
-    showSongList(/*add nessesary parameters here*/);
-    cout << endl;
-    
-
-
-    // Following code tests tsuPod input functionality 
-    int retCode = addSong("Runaway1", "Bon Jovi1", 1);
-//testing successful song addition
-    retCode = addSong(/*add nessesary parameters here*/,"Runaway2", "Bon Jovi2", 2);
-    showSongList(); // to show that I have added one song
-// I’m adding next six songs successfully 
-    retCode = addSong(/*add nessesary parameters here*/, "Runaway3", "Bon Jovi3", 3);
-    retCode = addSong(/*add nessesary parameters here*/, "Runaway4", "Bon Jovi4", 1);
-    retCode = addSong(/*add nessesary parameters here*/, "Runaway5", "Bon Jovi5", 5);
-    retCode = addSong(/*add nessesary parameters here*/, "Runaway6", "Bon Jovi6", 6);
-    retCode = addSong(/*add nessesary parameters here*/, "Runaway7", "Bon Jovi7", 1);
-    retCode = addSong(/*add nessesary parameters here*/, "Runaway8", "Bon Jovi8", 1);
-    retCode = addSong(/*add nessesary parameters here*/, "Runaway8", "Bon Jovi8", 1);
-    retCode = addSong(/*add nessesary parameters here*/, "Runaway8", "Bon Jovi8", 1);
-    showSongList(); //showing that I have added successfully 6 songs
-//testing unsuccessful song addition
-    retCode = addSong(/*add nessesary parameters here*/, "Runaway8", "Bon Jovi8", 1);
-    cout << retCode << “It was not possible to add songs because memory limit was exceeded” <<endl;   
-    showSongList();
+    // Initialize the tsuPod
+    initTsuPod(tsuPod, totalMemoryUsed);
+    cout << "Initialized tsuPod:" << endl;
+    showSongList(tsuPod);
     cout << endl;
 
-
-
-    // Following code tests tsuPod song removal functionality         
-    retCode = removeSong("Runaway100");
-    cout << retCode << endl;    
-    showSongList();
+    // Test successful song addition
+    cout << "Adding songs..." << endl;
+    addSong(tsuPod, totalMemoryUsed, "Runaway1", "Bon Jovi1", 1);
+    addSong(tsuPod, totalMemoryUsed, "Runaway2", "Bon Jovi2", 2);
+    addSong(tsuPod, totalMemoryUsed, "Runaway3", "Bon Jovi3", 3);
+    addSong(tsuPod, totalMemoryUsed, "Runaway4", "Bon Jovi4", 1);
+    addSong(tsuPod, totalMemoryUsed, "Runaway5", "Bon Jovi5", 5);
+    addSong(tsuPod, totalMemoryUsed, "Runaway6", "Bon Jovi6", 6);
+    addSong(tsuPod, totalMemoryUsed, "Runaway7", "Bon Jovi7", 1);
+    addSong(tsuPod, totalMemoryUsed, "Runaway8", "Bon Jovi8", 1);
+    showSongList(tsuPod);
     cout << endl;
 
-
-
-    // Following code tests tsuPod song shuffling functionality      
-    shuffle();
+    // Test unsuccessful song addition (memory limit exceeded)
+    cout << "Attempting to add another song (should fail due to memory limit):" << endl;
+    int retCode = addSong(tsuPod, totalMemoryUsed, "Runaway9", "Bon Jovi9", 6);
+    if (retCode == -1) {
+        cout << "Not enough memory to add song (Total memory: 25), occupied: "<<totalMemoryUsed << endl;
+    }
+    showSongList(tsuPod);
     cout << endl;
-    showSongList();
+
+    // Test unsuccessful song addition (no empty slots)
+    cout << "Attempting to add another song (should fail due to no empty slots):" << endl;
+    retCode = addSong(tsuPod, totalMemoryUsed, "Runaway10", "Bon Jovi10", 1);
+    if (retCode == -2) {
+        cout << "No empty slots available, total slots:8" << endl;
+    }
+    showSongList(tsuPod);
     cout << endl;
-    
-    system("PAUSE");
-    
+
+    // Test successful song removal
+    cout << "Removing song 'Runaway1':" << endl;
+    retCode = removeSong(tsuPod, totalMemoryUsed, "Runaway1");
+    if (retCode == 0) {
+        cout << "Song 'Runaway1' removed successfully" << endl;
+    }
+    showSongList(tsuPod);
+    cout << endl;
+
+    // Test unsuccessful song removal (song not found)
+    cout << "Attempting to remove song 'Runaway100' (should fail):" << endl;
+    retCode = removeSong(tsuPod, totalMemoryUsed, "Runaway100");
+    if (retCode == -1) {
+        cout << "Return code: " << retCode << " (Expected: -1)" << endl;
+        cout << "Song 'Runaway100' not found" << endl;
+    }
+    showSongList(tsuPod);
+    cout << endl;
+
+    // Test clearing the song list
+    cout << "Clearing the song list:" << endl;
+    clearMemory(tsuPod, totalMemoryUsed);
+    showSongList(tsuPod);
+    cout << endl;
+
+    // Test shuffle functionality
+    cout << "Adding songs for shuffle test..." << endl;
+    addSong(tsuPod, totalMemoryUsed, "Song1", "Artist1", 1);
+    addSong(tsuPod, totalMemoryUsed, "Song2", "Artist2", 2);
+    addSong(tsuPod, totalMemoryUsed, "Song3", "Artist3", 3);
+    cout << "Before shuffle:" << endl;
+    showSongList(tsuPod);
+    cout << endl;
+
+    cout << "After shuffle:" << endl;
+    shuffle(tsuPod);
+    showSongList(tsuPod);
+    cout << endl;
+
+    return 0;
 }
